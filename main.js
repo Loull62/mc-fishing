6f360934151d2a6670ca305430704897d52be33a
Loull62 <112659573+Loull62@users.noreply.github.com>
2022-09-07 21:20:48 -0600

    Update main.js

--- main.js
+++ main.js
@@ -26,43 +26,38 @@ function fishBtnClicked() {
   // Catch fish based on character
   if (character === 'steve') {
     // STEVE: Cod 70%, Salmon 20%, Tropical 5%, Puffer 5%
-    let randNum = Math.random();
-    if (randNum < 0.7) {
-      numCod++;
-      numCodSpan.innerHTML = numCod;
-      resultImg.src = 'img/Raw-Cod.png';
-    } else if (randNum < 0.9) {
-      numSalmon++;
-      numSalmonSpan.innerHTML = numSalmon;
-      resultImg.src = 'img/Raw-Salmon.png';
-    } else if (randNum < 0.95) {
-      numTropical++;
-      numTropicalSpan.innerHTML = numTropical;
-      resultImg.src = 'img/Tropical-Fish.png';
-    } else {
-      numPuffer++;
-      numPufferSpan.innerHTML = numPuffer;
-      resultImg.src = 'img/Pufferfish.png';
-    }
+    catchFish(0.7, 0.2, 0.05, 0.05)
+
   } else if (character === 'alex') {
     // ALEX: Cod 10%, Salmon 10%, Tropical 30%, Puffer 50%
-    let randNum = Math.random();
-    if (randNum < 0.1) {
-      numCod++;
-      numCodSpan.innerHTML = numCod;
-      resultImg.src = 'img/Raw-Cod.png';
-    } else if (randNum < 0.2) {
-      numSalmon++;
-      numSalmonSpan.innerHTML = numSalmon;
-      resultImg.src = 'img/Raw-Salmon.png';
-    } else if (randNum < 0.5) {
-      numTropical++;
-      numTropicalSpan.innerHTML = numTropical;
-      resultImg.src = 'img/Tropical-Fish.png';
-    } else {
-      numPuffer++;
-      numPufferSpan.innerHTML = numPuffer;
-      resultImg.src = 'img/Pufferfish.png';
-    }
+    catchFish(0.1, 0.1, 0.3, 0.5)
   }
+}
+
+function catchFish(codChance, salmonChance, tropicalChance, pufferChance) {
+  
+}
+
+function catchCod() {
+  numCod++;
+  numCodSpan.innerHTML = numCod;
+  resultImg.src = 'img/Raw-Cod.png';
+}
+
+function catchSalmon() {
+  numSalmon++;
+  numSalmonSpan.innerHTML = numSalmon;
+  resultImg.src = 'img/Raw-Salmon.png';
+}
+
+function catchTropical() {
+  numTropical++;
+  numTropicalSpan.innerHTML = numTropical;
+  resultImg.src = 'img/Tropical-Fish.png';
+}
+
+function catchPuffer() {
+  numPuffer++;
+  numPufferSpan.innerHTML = numPuffer;
+  resultImg.src = 'img/Pufferfish.png';
 }
